d35de0c68ffabf4605b1081887c94fc443f71f72
kothaunaing <141340019+kothaunaing@users.noreply.github.com>
2023-11-16 23:48:31 +0630

    Add files via upload

--- script.js
+++ script.js
@@ -1,186 +1,232 @@
-const nameInputElement = document.querySelector('.name-input');
-const displayPeopleElement = document.querySelector('.display-people');
-const addedPeopleElement = document.querySelector('.added-people');
-const numberPeopleInputElement = document.querySelector('.people-number-input');
-const selectedPeopleElement = document.querySelector('.selected-people');
-const chosenPeopleElement = document.querySelector('.chosen-message');
-
 let addedPeople = JSON.parse(localStorage.getItem('added-people')) || [];
-let chosenPeople = JSON.parse(localStorage.getItem('chosen-people')) || [];
+let pickedPeople = JSON.parse(localStorage.getItem('picked-people')) || [];;
 
-displayAddedPeople();
-displayChosenPeople();
-chosenPeople = [];
+displayAddedPeople(); // Display all people added in the list
+displayPickedPeople();
 
-function addPeople() {
-  const addedPerson = nameInputElement.value;
+document.querySelector('.add-btn') // When click add button the following code will work
+  .addEventListener('click', () => {
+    addToList();
+    displayAddedPeople();
+    saveToLocalStorage();
+  });
 
-  if (addedPerson) {
-    addedPeople.push(addedPerson);
+document.querySelector('.name-input').addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    addToList();
+    displayAddedPeople();
+    saveToLocalStorage();
   }
+})
 
-  displayAddedPeople();
-  nameInputElement.value = '';
-  saveToLocal();
-}
+document.querySelector('.choose-btn')
+  .addEventListener('click', () => {
+    pickPeople();
 
-function displayAddedPeople() {
-  let addedPeopleDisplay = '';
+    setTimeout(() => {
+      displayPickedPeople();
+    }, 2000);
 
-  for (let i = 0; i < addedPeople.length; i++) {
-    const name = addedPeople[i];
+    document.querySelector('.selected-people').innerHTML = '<p class="picking">Picking randomly . . .</p>';
+    setTimeout(() => {
+      document.querySelector('.picking').remove() = '';
+    }, 2000)
 
-    addedPeopleDisplay += `
-       <div class="added-person-container">
-          <div class="added-person">
-            <div class="number">${i + 1}.</div>
-            <div class="name">${name}</div>
-          </div>
-            <button class="delete-btn" onclick="
-            addedPeople.splice(${i}, 1);
-            displayAddedPeople();
-            saveToLocal();
-          ">Remove</button>
-        </div>
-        `;
+    saveToLocalStorage();
+  });
+
+
+document.querySelector('.people-number-input').addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    pickPeople();
+    displayPickedPeople();
+    saveToLocalStorage();;
   }
-  displayPeopleElement.innerHTML = `${addedPeople.length} `;
+})
+
+
+function displayAddedPeople() { // Generate html content based on the added people array
+  const addedPeopleElement = document.querySelector('.added-people');
+  finalHTML = '';
+
   if (addedPeople.length !== 0) {
+    addedPeople.forEach((name, index) => {
+      finalHTML += `
+      <div class="added-person-container">
+      <div class="added-person">
+        <div class="number">${index + 1}.</div>
+        <div class="name">${name}</div>
+      </div>
+        <button class="delete-btn js-delete-btn" 
+        data-name="${name}">
+          Remove
+        </button>
+    </div>
+      `;
+    });
+
     addedPeopleElement.innerHTML = `
     <div class="clear-btn-container">
-      <button class="clear-btn" onclick="
-        clearAddedPeople();
-        ">
-          Remove All
+      <button class="clear-btn">
+        Remove All
       </button>
+      <div class="confirm-dialogue">
+        <div>Remove all ? </div>
+        <button class="yes-btn">Yes</button>
+        <button class="no-btn">No</button>
+      </div>
     </div>
-  ${addedPeopleDisplay}
-  `;
+    ${finalHTML}`;
+
+    document.querySelectorAll('.js-delete-btn') // Codes for remove button
+      .forEach((button, index) => {
+        button.addEventListener('click', () => {
+          addedPeople.splice(index, 1);
+          displayAddedPeople();
+          saveToLocalStorage();
+        });
+      });
+
+    const confirmDialogueElement = document.querySelector('.confirm-dialogue');
+    const removeAllButtonElement = document.querySelector('.clear-btn');
+
+    removeAllButtonElement
+      .addEventListener('click', () => {
+        if (!removeAllButtonElement.classList.contains('clear-btn-active')) {
+          removeAllButtonElement.classList.add('clear-btn-active');
+          confirmDialogueElement.classList.add('confirm-dialogue-active');
+        } else {
+          removeAllButtonElement.classList.remove('clear-btn-active');
+          confirmDialogueElement.classList.remove('confirm-dialogue-active');
+        }
+      });
+
+    document.querySelector('.yes-btn')
+      .addEventListener('click', () => {
+        addedPeople = [];
+        localStorage.removeItem('added-people');
+        displayAddedPeople();
+      });
+
+    document.querySelector('.no-btn')
+      .addEventListener('click', () => {
+        removeAllButtonElement.classList.remove('clear-btn-active');
+        confirmDialogueElement.classList.remove('confirm-dialogue-active');
+      });
+
+  } else {
+    addedPeopleElement.innerHTML = finalHTML;
   }
-  else {
-    addedPeopleElement.innerHTML = `
-    <p class="added-people-message">Added names will be shown here.</p>
-  `;
-  }
+  const displayPeopleElement = document.querySelector('.display-people');
+  displayPeopleElement.innerHTML = addedPeople.length;
 }
 
-function chooseButton() {
+const messageElement = document.querySelector('.message');
+function addToList() {
 
-  setTimeout(function(){
-    displayChosenPeople();
-    chosenPeople = [];
-    errorExceedPeople();
-  }, 2000);
+  nameInputElement = document.querySelector('.name-input');
+  const name = nameInputElement.value;
+  let matchingName = false;
 
-  chooseRandomPeople();
-  saveToLocal();
+  addedPeople.forEach((person) => {
+    if (name.toLowerCase() === person.toLowerCase()) {
+      matchingName = true;
+    }
+  });
 
-  selectedPeopleElement.innerHTML = `
-  <p class="chosen-people-message">
-      Loading . . .
-    </p>
-  `;
-}
-
-function chooseRandomPeople() {
-  let numberPeople = Number(numberPeopleInputElement.value);
-
-  if (isNaN(numberPeople) || numberPeople === 0) {
-    numberPeople = 1;
-    numberPeopleInputElement.value = '1';
-  }
-
-  const copyAddedPeople = addedPeople.slice();
-
-  if (numberPeople <= addedPeople.length) {
-    for (let i = 0; i < numberPeople; i++) {
-      let index = Math.floor(Math.random() * copyAddedPeople.length);
-
-      chosenPeople.push(copyAddedPeople[index]);
-      copyAddedPeople.splice(index, 1);
+  if (matchingName) {
+    message();
+    messageElement.innerHTML = 'This name is already in the list !';
+  } else {
+    if (name) {
+      addedPeople.push(name);
+    } else {
+      message();
+      messageElement.innerHTML = 'Name is empty !';
     }
   }
-}
-
-function displayChosenPeople() {
-  let chosenPeopleDisplay = '';
-  let people = 'people are';
-
-  if (chosenPeople.length <= 1) {
-    people = 'person is';
-  }
-
-  for (let i = 0; i < chosenPeople.length; i++) {
-    let name = chosenPeople[i];
-
-    chosenPeopleDisplay += `
-    <div class="selected-person">
-    <div class="number">${i + 1}.</div>
-    <div class="name">${name}</div>
-   </div>
-    `;
-  }
-  if (chosenPeople.length !== 0) {
-    selectedPeopleElement.innerHTML = `
-    <p class="chosen-message">${chosenPeople.length} ${people} picked randomly.</p>
-    ${chosenPeopleDisplay}
-    `;
-  }
-  else {
-    selectedPeopleElement.innerHTML = `
-    <p class="chosen-people-message">
-      Randomly picked names will be shown here.
-    </p>
-    `;
-  }
-}
-
-function saveToLocal() {
-  const chosenPeopleString = JSON.stringify(chosenPeople);
-  const addedPeopleString = JSON.stringify(addedPeople);
-  localStorage.setItem('added-people', addedPeopleString);
-  localStorage.setItem('chosen-people', chosenPeopleString);
-}
-
-function addWithEnter(event) {
-  if (event.key === 'Enter') {
-    addPeople();
-  }
-}
-
-function clearAddedPeople() {
-  localStorage.removeItem('added-people');
-  localStorage.removeItem('chosen-people');
-  addedPeople = [];
-  chosenPeople = [];
   displayAddedPeople();
-  displayChosenPeople();
   nameInputElement.value = '';
 }
 
-function errorExceedPeople() {
-  const peopleNumber = numberPeopleInputElement.value;
-  const totalPeopleNumber = addedPeople.length;
-  let message = `There are only ${totalPeopleNumber} people in the list.`;
+function pickPeople() {
+  pickedPeople = [];
+  const peopleNumberInputElement = document.querySelector('.people-number-input');
+  const peopleNumber = Number(peopleNumberInputElement.value);
+  let errormessage;
 
-  if (totalPeopleNumber === 1) {
-    message = `There is only one person in the list.`;
+  const addedPeopleCopy = addedPeople.slice();
+
+  if (peopleNumber <= addedPeople.length) {
+    for (let i = 0; i < peopleNumber; i++) {
+      let index = Math.floor(Math.random() * addedPeopleCopy.length);
+      pickedPeople.push(addedPeopleCopy[index]);
+      addedPeopleCopy.splice(index, 1);
+    }
+  } else {
+    if (addedPeople.length === 0) {
+      errormessage = `Add names first. Then pick again !`;
+    }
+    else if (addedPeople.length === 1) {
+      errormessage = `There is only one person in the list !`;
+    }
+    else {
+      errormessage = `There are only ${addedPeople.length} people in the list !`
+    }
+    message();
+    messageElement.innerHTML = errormessage;
   }
-
-  if (totalPeopleNumber === 0) {
-    message = `Add names first. Then pick again !`;
-  }
-
-  if (peopleNumber > totalPeopleNumber) {
-    selectedPeopleElement.innerHTML = `
-    <p class="chosen-people-message">
-    ${message}
-    </p> `;
+  if (!peopleNumber) {
+    message();
+    messageElement.innerHTML = 'Enter number of people to pick !';
   }
 }
 
-function pickWithEnter(event) {
-  if (event.key === 'Enter') {
-    chooseButton();
+function displayPickedPeople() {
+  const selectedPeopleElement = document.querySelector('.selected-people');
+  let finalHTML = '';
+
+  pickedPeople.forEach((name, index) => {
+    finalHTML += `
+    <div class="selected-person">
+      <div class="number">${index + 1}.</div>
+      <div class="name">${name}</div>
+   </div>
+    `;
+  });
+  let message;
+
+  if (pickedPeople.length === 1){
+    message = 'person';
   }
+  else {
+    message = 'people';
+  }
+
+  if (pickedPeople.length !== 0) {
+    selectedPeopleElement.innerHTML = `
+  <p class="chosen-message">Picked ${pickedPeople.length} ${message} randomly.</p>
+  ${finalHTML}
+  `;
+  }
+}
+
+function saveToLocalStorage() {
+  const addedPeopleString = JSON.stringify(addedPeople);
+  const pickedPeopleString = JSON.stringify(pickedPeople);
+
+  localStorage.setItem('added-people', addedPeopleString);
+  localStorage.setItem('picked-people', pickedPeopleString);
+}
+
+let timeOutId;
+
+function message() {
+  const messageElement = document.querySelector('.message-container');
+  messageElement.classList.add('message-container-active');
+
+  clearTimeout(timeOutId);
+  timeOutId = setTimeout(() => {
+    messageElement.classList.remove('message-container-active');
+  }, 3000);
 }
